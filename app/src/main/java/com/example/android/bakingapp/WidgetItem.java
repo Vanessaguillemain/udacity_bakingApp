34acde681c8f8da8c04a0b0d377cdb2a81d7ab89
vanessa <vanessa.guillemain@gmail.com>
2019-02-19 16:54:13 +0100

    Widget StackView fonctionne. Pas beau et pas branché.

---
+++ app/src/main/java/com/example/android/bakingapp/WidgetItem.java
@@ -0,0 +1,13 @@
+package com.example.android.bakingapp;
+
+/**
+ * Created by vanessa on 19/02/2019.
+ */
+
+public class WidgetItem {
+    public String text;
+
+    public WidgetItem(String text) {
+        this.text = text;
+    }
+}
